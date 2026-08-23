d634e124626c0fa27c565e19cd7689dd8692f8f0
Min Hein Aung <minheinaung97@gmail.com>
2026-08-23 21:53:28 +0800

    Fix resource ref and add AI Foundry purge on destroy
    Corrected an invalid resource reference (removed redundant `resource.` prefix) for the Application Insights connection name. Added an `azapi_resource_action` to purge the AI Foundry (Cognitive Services) account when the resource group is destroyed, since the standard deletedAccounts resource type is incompatible with this API version.

--- workshop/Infra/main.tf
+++ workshop/Infra/main.tf
@@ -84,7 +84,7 @@ module "ai_foundry" {
 resource "azapi_resource" "app_insights_connection" {
   type      = "Microsoft.CognitiveServices/accounts/connections@2026-05-01"
   parent_id = module.ai_foundry.ai_foundry_id
-  name      = "${resource.azurerm_application_insights.this.name}-conn"
+  name      = "${azurerm_application_insights.this.name}-conn"
 
   body = {
     properties = {
@@ -101,4 +101,13 @@ resource "azapi_resource" "app_insights_connection" {
       }
     }
   }
+}
+
+# If we use Microsoft.CognitiveServices/locations/resourceGroups/deletedAccounts, we will get
+# invalid configuration: expect `type` to be Microsoft.Resources/resourceGroups/deletedAccounts
+resource "azapi_resource_action" "purge_ai_foundry" {
+  method      = "DELETE"
+  resource_id = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/providers/Microsoft.CognitiveServices/locations/${azurerm_resource_group.this.location}/resourceGroups/${azurerm_resource_group.this.name}/deletedAccounts/${module.naming.cognitive_account.name_unique}"
+  type        = "Microsoft.Resources/resourceGroups/deletedAccounts@2021-04-30"
+  when        = "destroy"
 }
